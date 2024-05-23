b55f5f6b1eaaa9cb11a70767e24100e7083863bf
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-05-22 22:59:47 -0500

    Authentication with hard coded userid and password

--- src/component/todo/ToDoApp.jsx
+++ src/component/todo/ToDoApp.jsx
@@ -14,30 +14,54 @@ export default function ToDoApp(){
 
 function LoginComponent(){
     const [username,setUserName] =useState("")
+    const [password,setPassword]=useState("")
+    const [loginSuccess,setLoginSuccess]=useState(false)
+    const [loginFail,setLoginFail]=useState(false)
 
     function handleUserNameChange(event){
         setUserName(event.target.value)
     }
 
+    function handlePasswordChange(event){
+        setPassword(event.target.value)
+    }
+
+    function handleLogin(){
+        if(username==='sai' && password==='dummy'){
+            setLoginSuccess(true)
+            setLoginFail(false)
+        }
+        else{
+            setLoginFail(true)
+            setLoginSuccess(false)
+        }
+    }
+
+
     return(
         <div className="Login">
             <div class="loginForm">
+                {loginSuccess&&<div>Login Success</div>}
+                {loginFail&&<div>Login Failed</div>}
                 <div>
                     <label>Username</label>
                     <input type="text" name="username" value={username} onChange={handleUserNameChange}></input>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password"></input>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}></input>
                 </div>
                 <div>
-                    <button type="button">Login</button>
+                    <button type="button" onClick={handleLogin}>Login</button>
                 </div>
 
             </div>
         </div>
     )
 }
+
+
+
 
 function WelcomeComponent(){
     return(
